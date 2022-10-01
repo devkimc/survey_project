ec6f3d550663ce80d9c8f0b04f940c830642b242
franco <kws60000@naver.com>
2022-10-01 14:27:13 +0900

    style: font 설정

--- src/pages/InfoPage.style.ts
+++ src/pages/InfoPage.style.ts
@@ -19,7 +19,7 @@ export const InfoHeader = styled.div`
 
 /* Main */
 export const InfoTitle = styled.span`
-  font-family: "NotoSansKR-Bold";
+  font-family: "NotoSansCJKkr-Bold";
   font-size: 16px;
   color: ${(props) => props.theme.black};
 `;
@@ -29,13 +29,12 @@ export const InfoSubTitle = styled.div`
 `;
 
 export const InfoSubTitleTxt = styled.span`
-  font-family: "NotoSansKR-Bold";
+  font-family: "NotoSansCJKkr-Bold";
   font-size: 20px;
   color: ${(props) => props.theme.black};
 `;
 
 export const InfoExplain = styled.span`
-  font-family: "NotoSansKR-Regular";
   font-size: 14px;
   color: ${(props) => props.theme.gray};
 `;
@@ -48,17 +47,18 @@ export const InfoFooter = styled.div`
 export const SurveyCountExplain = styled.div``;
 
 export const SurveyCountTxt = styled.span<SurveyCountTxtProps>`
-  font-family: "NotoSansKR-Bold";
+  font-family: "NotoSansCJKkr-Bold";
   font-size: 16px;
   color: ${(props) =>
     props.isNumber ? props.theme.yellow : props.theme.black};
 `;
 
 export const SurveyStartButton = styled.button`
-  font-family: "NotoSansKR-Bold";
+  font-family: "NotoSansCJKkr-Bold";
   font-size: 14px;
   color: ${(props) => props.theme.black};
   background-color: ${(props) => props.theme.yellow};
   border-radius: 23px;
   height: 50px;
+  width: 100%;
 `;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,17 +2,23 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     @font-face {
-        font-family: 'NotoSansKR-Bold';
-        src: url('assets/fonts/NotoSansKR-Bold.otf') format('otf');
+        font-family: 'NotoSansCJKkr-Bold';
+        src: url('fonts/NotoSansCJKkr-Bold.otf') format('opentype');
     }
     @font-face {
-        font-family: 'NotoSansKR-Regular';
-        src: url('assets/fonts/NotoSansKR-Regular.otf') format('otf');
+        font-family: 'NotoSansCJKkr-Regular';
+        src: url('fonts/NotoSansCJKkr-Regular.otf') format('opentype');
     }
 
     body {
+        font-family: 'NotoSansCJKkr-Regular';
         min-width: 300px;
         margin: 0 30px;
+    }
+
+    button {
+        border: none;
+        cursor: pointer;
     }
 
 `;
